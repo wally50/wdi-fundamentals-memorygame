6bf0a443fd186e1f9df7d6c419b37cc0ac188c07
wally50 <wallyvdberg@gmail.com>
2017-11-10 11:10:49 +1100

    created game logic card flip

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 console.log("Up and running!");
-var cardOne = 'queen';
-var cardTwo = 'queen';
-var cardThree = 'king';
-var cardFour = 'king';
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[1];
+cardsInPlay.push(cardOne);
+console.log('User flipped queen');
+var cardTwo = cards[0];
+cardsInPlay.push(cardTwo);
+console.log('User flipped king');
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {alert("You found a match!")} else {alert("Sorry, try again.");}
+}
