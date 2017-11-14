2f4cde2e518c964e4a98c4d8bca21ec62acd65c0
wally50 <wallyvdberg@gmail.com>
2017-11-14 12:54:45 +1100

    flipcard function added

--- js/main.js
+++ js/main.js
@@ -1,13 +1,32 @@
-console.log("Up and running!");
+//console.log("Up and running!");
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[1];
-cardsInPlay.push(cardOne);
-console.log('User flipped queen');
-var cardTwo = cards[0];
-cardsInPlay.push(cardTwo);
-console.log('User flipped king');
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {alert("You found a match!")} else {alert("Sorry, try again.");}
-}
+var checkForMatch = function (){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	console.log("You found a match!");
+	} else {
+	console.log("Sorry, try again.");
+	}
+
+};
+
+var flipcard = function (cardId) {
+		cardsInPlay.push(cards[cardId]);
+
+			console.log ("User flipped " + cards[cardId]);
+	
+
+			if (cardsInPlay.length === 2) {
+				checkForMatch();
+			//if (cardsInPlay[0] === cardsInPlay[1]) {alert("You found a match!")} else {alert("Sorry, try again.")};
+			};	
+};
+
+flipcard(0);
+flipcard(2);
+
+
+
+
+
